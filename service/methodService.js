79b11c6a816c8c8138a48896828782e35e955085
Daniel Strano <stranoj@gmail.com>
2021-07-26 15:50:41 -0400

    #67: Method routes

---
+++ service/methodService.js
@@ -0,0 +1,45 @@
+// resultService.js
+
+// Data Access Layer
+const MongooseService = require('./mongooseService')
+// Database Model
+const MethodModel = require('../model/methodModel')
+
+class MethodService {
+  constructor () {
+    this.MongooseServiceInstance = new MongooseService(MethodModel)
+  }
+
+  async create (methodToCreate) {
+    try {
+      const result = await this.MongooseServiceInstance.create(methodToCreate)
+      return { success: true, body: result }
+    } catch (err) {
+      return { success: false, error: err }
+    }
+  }
+
+  async getById (methodId) {
+    return await this.MongooseServiceInstance.find({ _id: methodId })
+  }
+
+  async getSanitized (methodId) {
+    const methods = await this.getById(methodId)
+    if (!methods || !methods.length || methods[0].isDeleted()) {
+      return { success: false, error: 'Method not found.' }
+    }
+    return { success: true, body: methods[0] }
+  }
+
+  async submit (userId, reqBody) {
+    const method = await this.MongooseServiceInstance.new()
+    method.user = userId
+    method.name = reqBody.name
+    method.fullName = reqBody.fullName
+    method.description = reqBody.description
+
+    return await this.create(method)
+  }
+}
+
+module.exports = MethodService
