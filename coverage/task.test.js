8d19c3cb4674183a6f220fb27cac9a969cf346f6
vprusso <vincentrusso1@gmail.com>
2021-07-29 14:48:01 -0400

    Adding unit tests for task.

--- coverage/task.test.js
+++ coverage/task.test.js
@@ -1,10 +1,8 @@
 // tests/task.test.js
 
-const mongoose = require('mongoose')
 const dbHandler = require('./db-handler');
 const TaskService = require('../service/taskService')
 const UserService = require('../service/userService.js')
-const SubmissionService = require('../service/submissionService');
 
 /**
  * Connect to a new in-memory database before running any tests.
@@ -24,57 +22,35 @@ afterAll(async () => await dbHandler.closeDatabase());
 /**
  * Task test suite.
  */
-describe('submission', () => {
+describe('task', () => {
 
     it('can be created', async () => {
         // Initialize
         const userId = (await (new UserService()).register(registration1)).body._id
         const taskService = new TaskService()
 
-        const result = await taskService.submit(userId, task1)
-        console.log(result)
-
         // Act
+        const result = await taskService.submit(userId, task1)
 
         // Assert
-//        expect(result.submissionCount)
-//            .toEqual(1)
+        expect(result.success).toBe(true)
     })
 
-    it('can be retrieved', async () => {
+    it('can be deleted', async () => {
         // Initialize
         const userId = (await (new UserService()).register(registration1)).body._id
-        const submissionService = new SubmissionService()
-        const submissionResult = await submissionService.submit(userId, submission1, false)
+        const taskService = new TaskService()
+        const taskResult = await taskService.submit(userId, task1)
 
         // Act
-        const result = await submissionService.get(submissionResult.body._id)
+        const result = await taskService.delete(taskResult.body._id)
 
         // Assert
-        expect(result.body)
-            .toMatchObject(submissionResponse1)
+        expect(result.success).toBe(true)
     })
 
+
 })
-
-const submission1 = {
-    submissionName: 'Test Submission',
-}
-
-const submission2 = {
-    submissionName: 'Test Submission 2',
-}
-
-const submissionResponse1 = {
-    submissionName: 'Test Submission',
-    submissionName: 'Test Submission',
-    submissionNameNormal: 'test submission'
-}
-
-const undefinedSubmissionId = {
-    submissionName: 'Test',
-    id: mongoose.Types.ObjectId("60cbedcdf5cf30ca9d645ab7")
-}
 
 const registration1 = {
     username: 'Test1',
