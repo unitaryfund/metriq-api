dbf6f5ecff532c6ce7c9ea51b0f1e2cccb2e1903
Daniel Strano <stranoj@gmail.com>
2021-10-27 11:54:37 -0400

    Ignore errors

--- metriq-pull.sh
+++ metriq-pull.sh
@@ -2,11 +2,11 @@ cd /home/ubuntu/GitHub/metriq-api
 git stash
 git pull
 git stash apply
-npm i
+npm i 2>/dev/null
 
 cd ..
 cd metriq-app
 git stash
 git pull
 git stash apply
-npm i
+npm i 2>/dev/null
