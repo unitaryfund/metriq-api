8235df0caed36b149e12dc8b359508b03258664d
Daniel Strano <stranoj@gmail.com>
2021-11-09 13:46:34 -0500

    Fix metriq-start.sh

--- metriq-start.sh
+++ metriq-start.sh
@@ -1,5 +1,5 @@
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-cd /metriq-api
+cd metriq-api
 screen -S metriq-api -d -m nodemon index.html
 cd ../metriq-app
 screen -S metriq-app -d -m npm start
