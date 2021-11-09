d92aa7cdecf31d70e07d891a51c931c086cc61ac
Daniel Strano <stranoj@gmail.com>
2021-11-09 13:40:38 -0500

    Update metriq_start.sh

--- metriq-start.sh
+++ metriq-start.sh
@@ -1,9 +1,5 @@
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-
-cd /workspace/metriq-api
-npm i
-screen -dm nodemon index.html
-
-cd /workspace/metriq-app
-npm i
-screen -dm npm start
+cd /metriq-api
+screen -S metriq-api -d -m nodemon index.html
+cd ../metriq-app
+screen -S metriq-app -d -m npm start
