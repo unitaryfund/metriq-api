5f01520fde5276d52e1d09528bdc7092e02fc1c4
Daniel Strano <stranoj@gmail.com>
2022-02-15 09:24:44 -0500

    unitaryfund/metriq-app#411: isHideChart field on tasks

--- metriq-api/migrations/2-363_MethodHierarchy.js
+++ metriq-api/migrations/2-363_MethodHierarchy.js
@@ -2,17 +2,35 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('methods', 'methodId',
-      {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'methods',
-          key: 'id'
-        }
-      }
-    )
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('methods', 'methodId',
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'methods',
+              key: 'id'
+            }
+          },
+          { transaction: t }
+        ),
+        queryInterface.addColumn('tasks', 'isHideChart',
+          {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+          },
+          { transaction: t }
+        )
+      ])
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('methods', 'methodId')
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.removeColumn('tasks', 'isHideChart'),
+        queryInterface.removeColumn('methods', 'methodId')
+      ])
+    })
   }
 }
