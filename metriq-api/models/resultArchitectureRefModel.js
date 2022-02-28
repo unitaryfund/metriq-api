24fcfd3b81aab20e8a97a5760887387b8830f42e
Daniel Strano <stranoj@gmail.com>
2022-02-28 16:38:56 -0500

    unitaryfund/metriq-app#452: resultArchitectureRef model

---
+++ metriq-api/models/resultArchitectureRefModel.js
@@ -0,0 +1,11 @@
+// resultArchitectureRefModel.js
+
+'use strict'
+module.exports = function (sequelize, DataTypes) {
+  const Model = sequelize.define('resultArchitectureRef', {}, {})
+  Model.associate = function (db) {
+    db.user.hasMany(db.resultArchitectureRef)
+    db.resultArchitectureRef.belongsTo(db.result)
+  }
+  return Model
+}
