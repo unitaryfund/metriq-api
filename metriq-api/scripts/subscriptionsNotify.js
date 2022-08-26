352e6ce19ba7bf9a198a482200d1b4ce76c27bb7
Daniel Strano <stranoj@gmail.com>
2022-08-26 15:29:36 -0400

    unitaryfund/metriq-app#650: Import Sequelize in notifications script

---
+++ metriq-api/scripts/subscriptionsNotify.js
@@ -0,0 +1,9 @@
+// Import Sequelize
+import Sequelize from 'sequelize'
+// Get the connection string
+import config from '../config'
+
+(async () => {
+    console.log("Checking for user subscription notifications...")
+    const sequelize = new Sequelize(config.pgConnectionString, { logging: false })
+})()
