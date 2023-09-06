2bd91c8ad462d72323b04c55ac76c8342d265bf4
Dan Strano <stranoj@gmail.com>
2023-09-06 16:16:01 -0400

    #223: Providers/architectures seed

---
+++ metriq-api/seeders/20230906200535-providers.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('providers', [
+      { name: 'IBMQ', fullName: 'IBMQ', description: '', userId: 1 },
+      { name: 'Rigetti', fullName: 'Rigetti', description: '', userId: 1 },
+      { name: 'IonQ', fullName: 'IonQ', description: '', userId: 1 },
+      { name: 'ETH', fullName: 'ETH', description: '', userId: 1 },
+      { name: 'NVIDIA', fullName: 'NVIDIA', description: '', userId: 1 }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('providers', null, {})
+  }
+}
